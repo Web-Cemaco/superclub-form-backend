1657a73ea3e65f677702650edb40e0f9b9fc5889
andretirado <andre.tirado@cemaco.com>
2021-08-24 15:16:34 -0600

    body parse params con parametros

--- enviar_correo_superclub/app.js
+++ enviar_correo_superclub/app.js
@@ -11,14 +11,41 @@ exports.handler = async (event, context, callback) => {
         let body = JSON.parse(event.body);
         let params = event.queryStringParameters
         let superclubNombreNino = params.superclubNombreNino;
+        let superclubApellidos = params.superclubApellidos;
+        let superclubEmail = params.superclubEmail;
+        let superclubConfirmar = params.superclubConfirmar;
+        let superclubFechaNacimiento = params.superclubFechaNacimiento;
+        let superclubGeneroF = params.superclubGeneroF;
+        let superclubGeneroM = params.superclubGeneroM;
+        let superclubIntereses = params.superclubIntereses;
+
+        //Datos de encargado
+        let superclubParentesco = params.superclubParentesco;
+        let superclubNombresPapas = params.superclubNombresPapas;
+        let superclubApellidosPapas = params.superclubApellidosPapas;
+        let superclubDPI = params.superclubDPI;
+        let superclubNoPrivilegio = params.superclubNoPrivilegio;
+        let superclubFechaPapas = params.superclubFechaPapas;
+        let superclubGeneroPapasF = params.superclubGeneroPapasF;
+        let superclubGeneroPapasM = params.superclubGeneroPapasM;
+        let superclubCorreoPapas = params.superclubCorreoPapas;
+        let superclubConfirmarPapas = params.superclubConfirmarPapas;
+        let superclubTelefono = params.superclubTelefono;
+        let superclubTelefonoCelular = params.superclubTelefonoCelular;
+        let superclubZona = params.superclubZona;
+        let superclubDepartamento = params.superclubDepartamento;
+        let superclubDireccion = params.superclubDireccion;
+        let superclubDireccionMisma = params.superclubDireccionMisma;
+        let superclubDireccionOtra = params.superclubDireccionOtra;
         let msg = {
             to: "andre8tr@gmail.com",
             from: "andre.tirado@cemaco.com",
             subject: 'Prueba',
             text: 'Nueva inscripcion',
             templateId: 'd-5f7ac94ed1e04b38b19f4bff158e990b',
-            dynamic_template_data : {
-                superclubNombreNino: superclubNombreNino
+            dynamic_template_data: {
+                superclubNombreNino: superclubNombreNino,
+                superclubApellidos: superclubApellidos
 
             }
         }
